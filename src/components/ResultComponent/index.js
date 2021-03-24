2e2eb7476ef9358317c5bef4e781260463accb08
abbyb97 <bansodeabhijit1997@gmail.com>
2021-03-24 19:36:24 +0530

    added compressed img

--- src/components/ResultComponent/index.js
+++ src/components/ResultComponent/index.js
@@ -9,12 +9,13 @@ import {
 } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
-import resultImg from "../../dist/images/cardImages/resImg.jpg";
+import resultImg from "../../dist/images/cardImages/resImgMin.jpg";
 import FadeContainer from "../../widgets/FadeContainer";
 import { device } from "../../utils/mediaQuery";
 import { useDispatch, useSelector } from "react-redux";
 import HomeModal from "../../widgets/HomeModal";
 import { simpleAction, setDifficultyAction } from "../../redux/actions";
+import OnImagesLoaded from "react-on-images-loaded";
 
 const ResultComponent = () => {
   const history = useHistory();
@@ -25,37 +26,43 @@ const ResultComponent = () => {
     history.push("/");
   }
   return (
-    <FadeContainer>
-      <StyledHome>
-        <h3 className="mb-3">Result</h3>
-        <Container style={{ height: "100vh" }} className="card-container pb-3">
-          <Card>
-            <Card.Img
-              className="mx-auto mt-3 w-50"
-              variant="top"
-              src={resultImg}
-            />
-            <Card.Body style={{ minHeight: "" }}>
-              <Card.Title>Your score is : </Card.Title>
-              <Card.Text>Congrats on completing the test!</Card.Text>
-            </Card.Body>
-            <Container fluid>
-              <Row>
-                <Col>
-                  <Button
-                    className="w-100 mb-2"
-                    variant="primary"
-                    onClick={() => history.push("/")}
-                  >
-                    Take test again <i className="fas fa-angle-right"></i>
-                  </Button>
-                </Col>
-              </Row>
-            </Container>
-          </Card>
-        </Container>
-      </StyledHome>
-    </FadeContainer>
+    <OnImagesLoaded>
+      {" "}
+      <FadeContainer>
+        <StyledHome>
+          <h3 className="mb-3">Result</h3>
+          <Container
+            style={{ height: "100vh" }}
+            className="card-container pb-3"
+          >
+            <Card>
+              <Card.Img
+                className="mx-auto mt-3 w-50"
+                variant="top"
+                src={resultImg}
+              />
+              <Card.Body style={{ minHeight: "" }}>
+                <Card.Title>Your score is : </Card.Title>
+                <Card.Text>Congrats on completing the test!</Card.Text>
+              </Card.Body>
+              <Container fluid>
+                <Row>
+                  <Col>
+                    <Button
+                      className="w-100 mb-2"
+                      variant="primary"
+                      onClick={() => history.push("/")}
+                    >
+                      Take test again <i className="fas fa-angle-right"></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </Container>
+            </Card>
+          </Container>
+        </StyledHome>
+      </FadeContainer>
+    </OnImagesLoaded>
   );
 };
 
