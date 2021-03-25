7ed4571a2b302536b97f4857253a86ef5ad8bc8c
abbyb97 <bansodeabhijit1997@gmail.com>
2021-03-25 08:54:45 +0530

    added modal for bacjk

--- src/components/QuizComponent/index.js
+++ src/components/QuizComponent/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useHistory, useParams } from "react-router-dom";
 import {
   Container,
@@ -14,6 +14,7 @@ import styled from "styled-components";
 import { setScore } from "../../redux/actions";
 import FadeContainer from "../../widgets/FadeContainer";
 import { useDispatch } from "react-redux";
+import QuizModal from "../../widgets/QuizModal";
 
 const QuizComponent = () => {
   const quizReducer = useSelector((state) => state.quizReducer);
@@ -21,6 +22,7 @@ const QuizComponent = () => {
 
   const dispatch = useDispatch();
 
+  const [visible, setVisible] = useState(true);
   const [selected, setSelected] = useState(null);
   const history = useHistory();
   const { question } = useParams();
@@ -28,6 +30,17 @@ const QuizComponent = () => {
   if (quizReducer && quizReducer.difficulty == "") {
     history.push("/");
   }
+
+  useEffect(() => {
+    return () => {
+      if (history.action === "POP") {
+        // handle any state changes necessary to set the screen display back one page.
+        console.log("backbutton press");
+        history.push(`/quiz/${question}`);
+        setVisible(true);
+      }
+    };
+  }, [question]);
 
   const handleNext = () => {
     if (parseInt(question) + 1 !== questionSet.length) {
@@ -129,6 +142,11 @@ const QuizComponent = () => {
             </Card>
           </Container>
         </StyledQuizComp>
+        <QuizModal
+          animation={"slideDown"}
+          visible={visible}
+          setVisible={setVisible}
+        />
       </FadeContainer>
     )
   );
---
+++ src/widgets/QuizModal.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Rodal from "rodal";
+import { Container, Row, Button } from "react-bootstrap";
+// import confusedRobo from "../dist/images/cardImages/confusedRobo.png";
+import confusedRobo from "../dist/images/cardImages/confusedRobo.png";
+
+const QuizModal = ({ animation, visible, setVisible }) => {
+  return (
+    <Rodal
+      animation={animation}
+      visible={visible}
+      onClose={() => setVisible(false)}
+    >
+      <Container>
+        <Row>
+          <h5 className=" mt-4 mx-auto">You cannot go back from here</h5>
+        </Row>
+        <Row className="justify-content-center">
+          <img
+            className="w-25 h-25"
+            style={{ borderRadius: "1rem" }}
+            src={confusedRobo}
+            alt="robo"
+          />
+        </Row>
+        <Row className="justify-content-center">
+          <Button className="w-75 my-3" onClick={() => setVisible(false)}>
+            Alright, got it !
+          </Button>
+        </Row>
+      </Container>
+    </Rodal>
+  );
+};
+
+export default QuizModal;
